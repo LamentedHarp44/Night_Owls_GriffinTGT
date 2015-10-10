433a39bc184d040990a40a2a787ed81b54fe3c3b
Walter Kautsky <Walter.Kautsky@fullsail.edu>
2015-10-09 21:37:59 -0400

    Malformed patient introduction complete. The first one doesn't kill the player.

---
+++ Assets/TriggerInfoScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerInfoScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			GetComponent<SpriteRenderer>().enabled = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			GetComponent<SpriteRenderer>().enabled = false;
+		}
+	}
+
+}
