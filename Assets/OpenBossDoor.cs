73024b66a07bd82e31000c803e17c809a2c7eb93
LamentedHarp <longjof@woh.rr.com>
2015-10-16 23:24:43 -0400

    JFL - Huge change to the timmy fight.

---
+++ Assets/OpenBossDoor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenBossDoor : MonoBehaviour {
+
+	public Sprite open;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag ("Player")) 
+		{
+			GameObject.FindGameObjectWithTag ("Boss").GetComponent<BigTimmy> ().inArena = false;
+			GameObject.FindGameObjectWithTag ("Boss Door").GetComponent<BoxCollider2D> ().enabled = false;
+			GameObject.FindGameObjectWithTag ("Boss Door").GetComponent<SpriteRenderer> ().sprite = open;
+		}
+	}
+}
