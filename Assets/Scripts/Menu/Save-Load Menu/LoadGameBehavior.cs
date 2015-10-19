26a936bb9ccbdf2956748a09484bfe6fd0cdcd81
LamentedHarp <longjof@woh.rr.com>
2015-10-19 16:28:21 -0400

    JFL - Added a single line to the player controller that caps the application framerate at 60 to fix problems with running the game in at lower graphics qualities.

--- Assets/Scripts/Menu/Save-Load Menu/LoadGameBehavior.cs
+++ Assets/Scripts/Menu/Save-Load Menu/LoadGameBehavior.cs
@@ -15,26 +15,25 @@ public class LoadGameBehavior : MonoBehaviour {
 
 	public void LoadSave()
 	{
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().lives = PlayerPrefs.GetInt ("Lives");
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().loot = PlayerPrefs.GetInt ("Money");
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().lastCompleted = PlayerPrefs.GetInt ("Level");
-		if (PlayerPrefs.GetInt ("SavedVAPurchase") > 0);
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseVerticalAttachment ();
-		if (PlayerPrefs.GetInt ("SavedGAPurchase") >0);
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseGrabAttachment ();
-		if (PlayerPrefs.GetInt ("SavedBAPurchase") > 0);
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseBladeAttachment ();
-		if (PlayerPrefs.GetInt ("SavedCooldownReduction")> 0);
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseCooldownReduction ();
-		if (PlayerPrefs.GetInt ("SavedDurationIncrease") > 0);
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseDurationIncrease ();
-		if (PlayerPrefs.GetInt ("SavedTrueInvisible") > 0);
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseTrueInvisible ();
-		if (PlayerPrefs.GetInt ("SavedUndetectedSearch") > 0);
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseUndetectedSearch ();
-		PlayerPrefs.SetInt ("LightExpo", PlayerPrefs.GetInt ("SavedLightExpo"));
-
-		GameObject.FindGameObjectWithTag ("Loader").GetComponent<SceneLoader> ().lvl = "Hub World";
-		Application.LoadLevel("Loading Screen");
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().loot = PlayerPrefs.GetInt ("Money");
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().lastCompleted = PlayerPrefs.GetInt ("Level");
+		//if (PlayerPrefs.GetInt ("SavedVAPurchase") > 0);
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseVerticalAttachment ();
+		//if (PlayerPrefs.GetInt ("SavedGAPurchase") >0);
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseGrabAttachment ();
+		//if (PlayerPrefs.GetInt ("SavedBAPurchase") > 0);
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseBladeAttachment ();
+		//if (PlayerPrefs.GetInt ("SavedCooldownReduction")> 0);
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseCooldownReduction ();
+		//if (PlayerPrefs.GetInt ("SavedDurationIncrease") > 0);
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseDurationIncrease ();
+		//if (PlayerPrefs.GetInt ("SavedTrueInvisible") > 0);
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseTrueInvisible ();
+		//if (PlayerPrefs.GetInt ("SavedUndetectedSearch") > 0);
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().PurchaseUndetectedSearch ();
+		//PlayerPrefs.SetInt ("LightExpo", PlayerPrefs.GetInt ("SavedLightExpo"));
+		//GameObject.FindGameObjectWithTag ("Loader").GetComponent<SceneLoader> ().lvl = "Hub World";
+		//GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().lives = PlayerPrefs.GetInt ("Lives");
+		//Application.LoadLevel("Loading Screen");
 	}
 }
